c6890a4ce7b26ce716825e4e47ee096c4b528d2c
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-18 14:48:40 +0200

    ts

--- Practice21012022/TypeScript_Setup/src/app.ts
+++ Practice21012022/TypeScript_Setup/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Welcome to TS World!");
+
+const addTwo = (num2: number) => {
+  let num1 = 8;
+  return num1 + num2;
+};
+
+const divideTwo = (num1: number, num2: number) => {
+  return num1 % num2;
+};
+
+const multiplyTwo = (num1: number, num2: number) => {
+  return num1 * num2;
+};
+
+addTwo(7)
