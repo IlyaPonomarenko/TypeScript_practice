0ced6a61fe4d90154975d138de09d3b84fd69e73
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-25 10:54:10 +0200

    new project

--- Practice21012022/TypeScript_Setup/src/app.ts
+++ Practice21012022/TypeScript_Setup/src/app.ts
@@ -1,15 +1,25 @@
+// const addTwo = (num2: number) => {
+//   let num1 = 8;
+//   return num1 + num2;
+// };
 
-const addTwo = (num2: number) => {
-  let num1 = 8;
-  return num1 + num2;
-};
+// const divideTwo = (num1: number, num2: number) => {
+//   return num1 % num2;
+// };
 
-const divideTwo = (num1: number, num2: number) => {
-  return num1 % num2;
-};
+// const multiplyTwo = (num1: number, num2: number) => {
+//   return num1 * num2;
+// };
 
-const multiplyTwo = (num1: number, num2: number) => {
-  return num1 * num2;
-};
+// addTwo(7)
+class Product {
+  title: string;
+  price: number;
+  private isListed: boolean;
 
-addTwo(7)
+  constructor(name: string, pr: number) {
+    this.title = name;
+    this.price = pr;
+    this.isListed = true;
+  }
+}
