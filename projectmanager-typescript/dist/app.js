916c966b32294a6ba2d004f9ac4114658ed9bd3d
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-25 13:56:40 +0200

    validator

--- projectmanager-typescript/dist/app.js
+++ projectmanager-typescript/dist/app.js
@@ -1,5 +1,27 @@
 "use strict";
 console.log("PMApp in TS begins...");
+function validate(validateInput) {
+    let isValid = true;
+    if (validateInput.required) {
+        isValid = isValid && validateInput.value.toString().trim().length !== 0;
+    }
+    if (validateInput.minLength != null &&
+        typeof validateInput.value === "string") {
+        isValid && validateInput.value.length >= validateInput.minLength;
+    }
+    if (validateInput.maxLength != null &&
+        typeof validateInput.value === "string") {
+        isValid && validateInput.value.length >= validateInput.maxLength;
+    }
+    if (validateInput.min != null &&
+        typeof validateInput.value === "number") {
+        isValid = isValid && validateInput.value >= validateInput.min;
+    }
+    if (validateInput.max != null &&
+        typeof validateInput.value === "number") {
+        isValid = isValid && validateInput.value >= validateInput.max;
+    }
+}
 function autobind(_, descriptor) {
     const originalmethod = descriptor.value;
     const adjDescriptor = {
--- projectmanager-typescript/src/app.ts
+++ projectmanager-typescript/src/app.ts
@@ -1,4 +1,44 @@
 console.log("PMApp in TS begins...");
+interface Validator {
+  value: string | number;
+  required?: boolean;
+  minLength: number;
+  maxLength: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validateInput: Validator) {
+  let isValid = true;
+  if (validateInput.required) {
+    isValid = isValid && validateInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validateInput.minLength != null &&
+    typeof validateInput.value === "string"
+  ) {
+    isValid && validateInput.value.length >= validateInput.minLength;
+  }
+  if (
+    validateInput.maxLength != null &&
+    typeof validateInput.value === "string"
+  ) {
+    isValid && validateInput.value.length >= validateInput.maxLength;
+  }
+  if (
+    validateInput.min != null &&
+    typeof validateInput.value === "number"
+  ) {
+    isValid = isValid && validateInput.value >= validateInput.min;
+  }
+  if (
+    validateInput.max != null &&
+    typeof validateInput.value === "number"
+  ) {
+    isValid = isValid && validateInput.value >= validateInput.max;
+  }
+  
+}
 
 function autobind(_: any, descriptor: PropertyDescriptor) {
   const originalmethod = descriptor.value;
@@ -50,18 +90,20 @@ class projectInput {
     const enteredTitle = this.titleInputElement.value;
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
-    if( enteredTitle.trim().length === 0 ||
-    enteredDescription.trim().length === 0 ||
-     enteredPeople.trim().length === 0){
-        alert("invalid input")
-     }else{
-        return [enteredTitle, enteredDescription, +enteredPeople]
-     }
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("invalid input");
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
   }
-  private clearInputs(){
-    this.titleInputElement.value = ""; 
-    this.descriptionInputElement.value = ""; 
-    this.peopleInputElement.value = ""; 
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
   }
 
   private attach() {
@@ -73,10 +115,10 @@ class projectInput {
   private submitHandler(event: Event) {
     event.preventDefault();
     const userInput = this.gatherUserInput();
-    if (Array.isArray(userInput)){
-        const [title, desc, people] = userInput;
-        console.log(title, desc, people);
-        this.clearInputs()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
     }
   }
 }
