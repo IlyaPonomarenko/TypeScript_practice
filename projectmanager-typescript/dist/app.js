13027fca3c30ec48c58ab7a6351f0c587df0cc99
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-25 11:17:52 +0200

    proj init

--- projectmanager-typescript/dist/app.js
+++ projectmanager-typescript/dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('PMApp in TS begins...');
+console.log("PMApp in TS begins...");
+class projectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prjInput = new projectInput();
