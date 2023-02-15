80128c348b4c9c0e7e7cb16aa89213590e614704
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-15 12:24:01 +0200

    todo delete

--- ts_react22s/src/components/TodoList.tsx
+++ ts_react22s/src/components/TodoList.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 interface todoListItems {
-    items: {id:string, text:string}[],
+    items: { id: string, text: string }[];
+    onDeleteTodo: (id: string) => void;
 }
 
+
 const TodoList: React.FC<todoListItems> = (props) => {
-    
+
     return (
         <ul>
-            {props.items.map(todo => (<li key={todo.id}>{todo.text}</li>))}
+            {props.items.map(todo => (<li key={todo.id}>{todo.text}
+                <button onClick={props.onDeleteTodo.bind(null, todo.id)}>delete</button>
+            </li>))}
         </ul>
     );
 };
