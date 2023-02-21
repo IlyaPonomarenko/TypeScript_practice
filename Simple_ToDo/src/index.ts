ac3df3a6aefd57171d7ce1c9564346b415bbc47d
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-21 10:45:40 +0200

    simple add

--- Simple_ToDo/src/index.ts
+++ Simple_ToDo/src/index.ts
@@ -2,20 +2,30 @@ const list = document.querySelector<HTMLUListElement>("#list");
 const taskForm = document.querySelector<HTMLFormElement>("#task-form");
 const taskInput = document.querySelector<HTMLInputElement>("#task-input");
 
-type Task = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
-
 const uniqueId = () => {
   const date = Date.now().toString(36);
   const random = Math.random().toString(36);
   return date + random;
 };
 
-taskForm?.addEvenListener("submit", (e: { prevendDefault: () => Event }) => {
-  e.prevendDefault();
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+const saveToLocal = () => {
+  localStorage.setItem("tasks list", JSON.stringify(savedTasks));
+};
+
+const loadFromLocal = (): Task[] => {
+  const tasksJSON = localStorage.getItem("tasks list");
+  if (tasksJSON == null) return [];
+  return JSON.parse(tasksJSON);
+};
+
+taskForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
   if (taskInput?.value == "" || taskInput?.value == null) return;
 
   const task: Task = {
@@ -23,14 +33,26 @@ taskForm?.addEvenListener("submit", (e: { prevendDefault: () => Event }) => {
     title: taskInput.value,
     completed: false,
   };
+  savedTasks.push(task);
   addTask(task);
+  taskInput.value = "";
 });
 
 const addTask = (task: Task) => {
   const item = document.createElement("li");
   const label = document.createElement("label");
-  const checkbox = (document.createElement("input").type = "checkbox");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    console.log(savedTasks)
+    saveToLocal();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
   label.append(checkbox, task.title);
   item.append(label);
-  list?.append(item)
+  list?.append(item);
 };
+
+const savedTasks: Task[] = loadFromLocal();
+savedTasks.forEach(addTask)
